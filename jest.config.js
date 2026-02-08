b35da4abb580037a4b71741f1c94f11b28598473
Stew Eucen <stew.eucen@gmail.com>
2026-02-08 10:48:57 +0900

    Convert jest.config.js to ESM

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-'use strict'
-
-module.exports = {
+export default {
   setupFilesAfterEnv: [
     '@openreachtech/renchan-test-tools/lib/environment/setupAfterEnv.js',
     '<rootDir>/node_modules/@openreachtech/jest-constructor-spy/config/setupAfterEnv.js',
