0e0d180d07882c255e70524b4c4e50a36ba4dc9c
Stew Eucen <stew.eucen@gmail.com>
2026-02-10 01:57:25 +0900

    Retake type of global.jest.Expect

--- types/jest-expect.deepContaining.d.ts
+++ types/jest-expect.deepContaining.d.ts
@@ -1,14 +1,14 @@
-import {
-  ExpectMixins,
-} from '@openreachtech/renchan-test-tools/lib/types'
-
-interface EnhancedExpect<R = unknown>
-  extends
-    ExpectMixins<R>
-{}
+export interface DeepContainingExpectMixin<R = unknown> {
+  deepContaining(
+    received: any,
+    options?: {
+      convertsArray?: boolean,
+    }
+  ): jasmine.ObjectContaining | jasmine.ArrayContaining | any
+}
 
 declare global {
   namespace jest {
-    interface Expect extends EnhancedExpect {}
+    interface Expect extends DeepContainingExpectMixin {}
   }
 }
